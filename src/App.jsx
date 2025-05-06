79386276ab7eaa4dacf85d8613d935ec1b5dd5c8
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-06 12:12:07 +0530

    Adds a custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const useRepeatedly = (callback, initial, delay) => {
+  const [state, setState] = useState(initial);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(() => {
+    const interval = setInterval(() => setState(callback), delay);
 
-export default App
+    return () => clearInterval(interval);
+  });
+
+  return state;
+};
+
+const Timer = () => {
+  const count = useRepeatedly((prev) => prev + 1, 0, 1000);
+
+  return <span>Count {count}</span>;
+};
+
+const RandomNumber = () => {
+  const number = useRepeatedly(Math.random, 0, 1000);
+
+  return <span>Random {number}</span>;
+};
+
+const App = () => (
+  <>
+    <Timer />
+    <RandomNumber />
+  </>
+);
+
+export default App;
